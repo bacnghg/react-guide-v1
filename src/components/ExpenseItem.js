402f430a1ac25f45d22bb22d2b1f5b49afc3e614
bacnh cw <bacnh7427@co-well.vn>
2023-05-23 10:19:32 +0700

    add day/month/year

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
 import './ExpenseItem.css'
 function ExpenseItem(props) {
-  const expenseDate = new Date(2023, 25, 5)
+
+
+
+  const month = props.date.toLocaleString('en-US',{month: 'long'} );
+  const day = props.date.toLocaleString('en-US',{day: '2-digit'} );
+  const year = props.date.getFullYear();
   const expenseTitle = "Car Insurance";
   const expenseAmout = 222.54;
   return (
     <div className="expense-item">
-      <h2>{props.date.toDateString()}</h2>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      {/* <h2>{props.date.toDateString()}</h2> */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
