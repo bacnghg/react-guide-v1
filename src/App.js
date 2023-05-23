a66d478ba21ed432006ff82a79bc304a42720ec4
bacnh cw <bacnh7427@co-well.vn>
2023-05-23 09:14:09 +0700

    add props and call data

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ function App() {
       id: '2',
       title: 'Candy Crush',
       amount: 94.21,
-      date: new Date(2025, 7, 13)
+      date: new Date(2025, 8, 13)
     },
     {
       id: '3',
       title: 'Apple tivi',
       amount: 94.21,
-      date: new Date(2025, 7, 13)
+      date: new Date(2022, 5, 13)
     },
     {
       id: '4',
@@ -32,19 +32,19 @@ function App() {
       id: '5',
       title: 'Smart Watch',
       amount: 394.21,
-      date: new Date(2025, 7, 13)
+      date: new Date(2022, 3, 10)
     },
     {
       id: '6',
       title: 'Ipad 10',
       amount: 594.21,
-      date: new Date(2025, 7, 13)
+      date: new Date(2022, 3, 20)
     },
     {
       id: '7',
       title: 'Iphone 14 promax',
       amount: 1094.21,
-      date: new Date(2025, 7, 13)
+      date: new Date(2022, 5, 8)
     },
   ]
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,10 +5,10 @@ function ExpenseItem(props) {
   const expenseAmout = 222.54;
   return (
     <div className="expense-item">
-      <h2>{expenseDate.toISOString()}</h2>
+      <h2>{props.date.toDateString()}</h2>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${expenseAmout}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
