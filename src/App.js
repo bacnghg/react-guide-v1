54935f489d603b466f4d34cc79d9fe3f826c1206
bacnh7427 <daidt7417@co-well.vn>
2023-05-25 15:04:32 +0700

    refactor-code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 function App() {
 
   const expenses = [
@@ -51,41 +52,7 @@ function App() {
   return (
     <div>
       <h2>Let's Go React</h2>
-      <ExpenseItem 
-      title={expenses[0].title}
-      amount={expenses[0].amount}
-      date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[1].title}
-      amount={expenses[1].amount}
-      date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[2].title}
-      amount={expenses[2].amount}
-      date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[3].title}
-      amount={expenses[3].amount}
-      date={expenses[3].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[4].title}
-      amount={expenses[4].amount}
-      date={expenses[4].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[5].title}
-      amount={expenses[5].amount}
-      date={expenses[5].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[6].title}
-      amount={expenses[6].amount}
-      date={expenses[6].date}
-      ></ExpenseItem>
+     <Expenses items = {expenses} />
     </div>
   );
 }
