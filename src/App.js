ea57679140d659913435f960b077579559d081d2
bacnh cw <bacnh7427@co-well.vn>
2023-06-21 15:25:34 +0700

    add form inputs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 function App() {
 
   const expenses = [
@@ -60,7 +61,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's Go React</h2>
+      <NewExpense />
      <Expenses items = {expenses} />
     </div>
   );
